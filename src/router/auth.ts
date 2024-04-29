de0349cf757fed64fb2256add686a08246bc6d84
Wahyu Agus Arifin - IP <68760662+ItPohgero@users.noreply.github.com>
2024-04-29 16:20:27 +0700

    update

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -1,6 +1,9 @@
 import Elysia from "elysia";
 
 const r_auth = new Elysia({ prefix: '/auth' })
+    .onBeforeHandle(({ set }) => {
+        console.log(set);
+    })
     .post('/sign-in', () => 'Sign in')
     .post('/sign-up', () => 'Sign up')
     .get('/profile', () => 'profile')
