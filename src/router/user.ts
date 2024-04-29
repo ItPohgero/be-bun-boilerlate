7c8c91465d6749420cb109ace1310a3356a150c6
Wahyu Agus Arifin - IP <68760662+ItPohgero@users.noreply.github.com>
2024-04-29 16:29:35 +0700

    update

--- src/router/user.ts
+++ src/router/user.ts
@@ -1,6 +1,12 @@
 import Elysia from "elysia";
 
 const r_user = new Elysia({ prefix: '/main' })
-    .get('/dashboard', () => 'Sign in')
+    .derive(({ headers }) => {
+        const auth = headers['authorization']
+        return {
+            bearer: auth?.startsWith('Bearer ') ? auth.slice(7) : null
+        }
+    })
+    .get('/dashboard', (e) => e)
 
 export default r_user
