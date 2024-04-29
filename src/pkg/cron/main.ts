68293ab518af365bfaa56a3bff4b6f38a67b2fa3
Wahyu Agus Arifin - IP <68760662+ItPohgero@users.noreply.github.com>
2024-04-29 22:59:03 +0700

    update

--- src/pkg/cron/main.ts
+++ src/pkg/cron/main.ts
@@ -2,7 +2,7 @@ import cron from "@elysiajs/cron"
 
 export  const Cronjob = {
     Main: () => cron({
-        name: 'heartbeat',
+        name: 'cron_name',
         pattern: '*/50 * * * * *',
         run() {
             console.log("50 detik :", new Date())
