56282a34c0fdedf8fa80bb58c7e6b1c297a1eb28
Wahyu Agus Arifin - IP <68760662+ItPohgero@users.noreply.github.com>
2024-04-29 21:21:39 +0700

    update

--- src/app/handler/auth.ts
+++ src/app/handler/auth.ts
@@ -1,14 +1,32 @@
 import { AuthType, JwtType } from "../../types/jwt"
 import { SchemaAuthSignInType } from "../schema/auth"
 
-type SignPayload = {
+type SignInPayload = {
+    dto: SchemaAuthSignInType
+    jwt: JwtType
+    auth: AuthType['auth']
+}
+type SignUpPayload = {
     dto: SchemaAuthSignInType
     jwt: JwtType
     auth: AuthType['auth']
 }
 
 export const HandlerAuth = {
-    SignIn: async (payload: SignPayload) => {
+    SignIn: async (payload: SignInPayload) => {
+        const { dto, auth, jwt } = payload
+        auth.set({
+            value: await jwt.sign(dto),
+            httpOnly: true,
+            maxAge: 7 * 86400,
+            path: '/',
+        })
+        return {
+            ...dto,
+            token: auth.value
+        }
+    },
+    SignUp: async (payload: SignUpPayload) => {
         const { dto, auth, jwt } = payload
         auth.set({
             value: await jwt.sign(dto),
--- src/app/schema/auth.ts
+++ src/app/schema/auth.ts
@@ -17,3 +17,27 @@ export const SchemaAuthSignIn = {
         })
     })
 }
+
+
+export type SchemaAuthSignUpType = {
+    name: string
+    email: string
+    password: string
+}
+
+export const SchemaAuthSignUp = {
+    body: t.Object({
+        name: t.String({
+            format: 'email',
+            examples: "wahyu agus arifin"
+        },),
+        email: t.String({
+            format: 'email',
+            examples: "wahyu@gmail.com"
+        },),
+        password: t.String({
+            minLength: 4,
+            examples: "87#!Ab"
+        })
+    })
+}
